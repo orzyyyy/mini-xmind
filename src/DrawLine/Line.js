5d11a33f167e0f472f6d12cb5adff97dab78599c
zy410419243 <zy410419243@gmail.com>
2018-12-30 11:03:35 +0800

    pref: change default style

--- src/DrawLine/Line.js
+++ src/DrawLine/Line.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
-const defaultBorderColor = '#f00';
+const defaultBorderColor = '#ddd';
 const defaultBorderStyle = 'solid';
 const defaultBorderWidth = 1;
 
