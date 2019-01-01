4ee09fe7a492143dc9aed186aa11faecf8937f00
zy410419243 <zy410419243@gmail.com>
2019-01-01 16:23:00 +0800

    pref: refactor test case

--- src/line/__tests__/DrawLine.test.js
+++ src/line/__tests__/DrawLine.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { render, mount, shallow } from 'enzyme';
 import { SteppedLineTo } from '..';
 import Block from '../../tools/Block';
@@ -6,71 +7,52 @@ import Block from '../../tools/Block';
 describe('DrawLine', () => {
   it('DrawLine render correctly', () => {
     class Demo extends React.Component {
-      render() {
-        const style = {
-          delay: true,
-          borderColor: '#ddd',
-          borderStyle: 'solid',
-          borderWidth: 3,
-        };
+      constructor() {
+        super();
 
+        this.state = {
+          steps: [],
+        };
+      }
+
+      componentDidMount = () => {
+        let steps = [];
+
+        steps.push(
+          <SteppedLineTo
+            from={ReactDOM.findDOMNode(this.blockA)}
+            to={ReactDOM.findDOMNode(this.blockB)}
+            fromAnchor="bottom"
+            toAnchor="top"
+            borderColor="#ddd"
+            borderStyle="solid"
+            borderWidth={3}
+          />,
+        );
+
+        this.setState({ steps });
+      };
+
+      render() {
         return (
           <React.Fragment>
-            <Block className="stepped-A" top="50px" left="90px" color="#00f">
+            <Block
+              top="50px"
+              left="90px"
+              color="#00f"
+              ref={ref => (this.blockA = ref)}
+            >
               A
             </Block>
-            <Block className="stepped-B" top="150px" left="20px" color="#00f">
+            <Block
+              top="150px"
+              left="20px"
+              color="#00f"
+              ref={ref => (this.blockB = ref)}
+            >
               B
             </Block>
-            <Block className="stepped-C" top="150px" left="90px" color="#00f">
-              C
-            </Block>
-            <Block className="stepped-D" top="150px" left="160px" color="#00f">
-              D
-            </Block>
-            <Block className="stepped-E" top="50px" left="300px" color="#00f">
-              E
-            </Block>
-            <Block className="stepped-F" top="120px" left="300px" color="#00f">
-              F
-            </Block>
-            <SteppedLineTo
-              from="stepped-A"
-              to="stepped-B"
-              fromAnchor="bottom"
-              toAnchor="top"
-              {...style}
-            />
-            <SteppedLineTo
-              from="stepped-A"
-              to="stepped-C"
-              fromAnchor="bottom"
-              toAnchor="top"
-              {...style}
-            />
-            <SteppedLineTo
-              from="stepped-A"
-              to="stepped-D"
-              fromAnchor="bottom"
-              toAnchor="top"
-              {...style}
-            />
-            <SteppedLineTo
-              from="stepped-A"
-              to="stepped-E"
-              fromAnchor="right"
-              toAnchor="left"
-              orientation="h"
-              {...style}
-            />
-            <SteppedLineTo
-              from="stepped-A"
-              to="stepped-F"
-              fromAnchor="right"
-              toAnchor="left"
-              orientation="h"
-              {...style}
-            />
+            {this.state.steps}
           </React.Fragment>
         );
       }
