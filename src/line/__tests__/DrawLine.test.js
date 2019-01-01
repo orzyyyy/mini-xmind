ad0d1b23ad204010134436d4684aad0848a4875a
zy410419243 <zy410419243@gmail.com>
2019-01-01 14:51:53 +0800

    ci: fix test problem

--- src/line/__tests__/DrawLine.test.js
+++ src/line/__tests__/DrawLine.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, mount, shallow } from 'enzyme';
-import { SteppedLineTo, Block } from '..';
+import { SteppedLineTo } from '..';
+import Block from '../../tools/Block';
 
 describe('DrawLine', () => {
   it('DrawLine render correctly', () => {
