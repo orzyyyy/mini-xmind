7c097be44e41bdaba2f80fa6559183e52d0c4037
zy410419243 <zy410419243@gmail.com>
2019-01-03 17:01:32 +0800

    chore: add instruction for rename

--- src/line/index.js
+++ src/line/index.js
@@ -1,5 +1,7 @@
 // fork from https://github.com/kdeloach/react-lineto
 // because I must refactor for draggable
+
+// rename Line to core because circleci is case-sensitive
 import Line from './core';
 import LineTo from './LineTo';
 import SteppedLineTo from './SteppedLineTo';
