23355b9c3ce9b49b435be845a592b7675a1cd342
zy410419243 <zy410419243@gmail.com>
2019-01-18 16:32:11 +0800

    chore: fix typo

--- nino.koei.js
+++ nino.koei.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    'lib/Canvas/index': path.join(__dirname + '/src/canvas'),
+    'lib/canvas/index': path.join(__dirname + '/src/canvas'),
   },
   output: {
     path: path.join(__dirname + '/dist'),
