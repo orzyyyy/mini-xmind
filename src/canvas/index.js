348c23ad922e9a6a171717e24fe415b60c564a2a
zy410419243 <zy410419243@gmail.com>
2019-01-20 15:33:18 +0800

    fix: can't get output data when using relase

--- src/canvas/index.js
+++ src/canvas/index.js
@@ -1,4 +1,7 @@
 // rename Canvas to core because circleci is case-sensitive
 import Canvas from './core';
+import Listener from '../utils/GlobalListener';
+
+window.DataCollector = new Listener();
 
 export default Canvas;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Listener from './utils/GlobalListener';
-
 import Demo from './demo';
 
 const MOUNT_NODE = document.getElementById('root');
-
-window.DataCollector = new Listener(MOUNT_NODE);
 
 ReactDOM.render(<Demo />, MOUNT_NODE);
 
