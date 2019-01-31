ee70ed80f8faae2018d6bc43fb552802998ff7d1
zy410419243 <zy410419243@gmail.com>
2019-01-31 08:32:04 +0800

    chore: release with source-map

--- scripts/nino.koei.js
+++ scripts/nino.koei.js
@@ -2,6 +2,8 @@ const path = require('path');
 const cwd = process.cwd();
 
 module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
   entry: {
     'lib/canvas': path.join(cwd + '/src/canvas'),
     'lib/toolbar': path.join(cwd + '/src/tools/Toolbar'),
