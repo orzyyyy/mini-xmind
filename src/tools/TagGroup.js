71848f25c9404d23e86dd1537f30e46b7536dda3
zy410419243 <zy410419243@gmail.com>
2019-02-02 14:13:53 +0800

    fix: can't type inside Input, close #18

--- src/tools/TagGroup.js
+++ src/tools/TagGroup.js
@@ -71,7 +71,7 @@ export default class TagGroup extends Component {
             {...rest}
           >
             <Input
-              className="animate-appear"
+              className="animate-appear not-draggable"
               onChange={e =>
                 this.handleChange(data[key], key, 'input', e.target.value)
               }
@@ -92,6 +92,7 @@ export default class TagGroup extends Component {
           key={key}
           position={{ x, y }}
           onDrag={e => onChange && onChange(data)}
+          cancel=".not-draggable"
         >
           <div
             className={classNames('TagGroup', className, 'animate-appear')}
