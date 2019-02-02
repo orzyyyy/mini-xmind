59a5b5cf900f6b8fd6caaac3fe72a6e478ad141d
zy410419243 <zy410419243@gmail.com>
2019-02-02 16:20:39 +0800

    test: update cases

---
+++ src/line/__tests__/Line.test.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { SteppedLineTo } from '..';
+import 'nino-cli/scripts/setup';
+
+const from = document.createElement('div');
+from.innerHTML = `
+  <div style="position: absolute; top: 0px; left: 0px;">from</div>
+`;
+
+const to = document.createElement('div');
+to.innerHTML = `
+<div style="position: absolute; top: 0px; left: 100px;">from</div>
+`;
+
+describe('Line', () => {
+  it('Line renders correctly', () => {
+    const wrapper = mount(
+      <SteppedLineTo from={from} to={to} key="line-test" orientation="v" />,
+    );
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('.stepped-line-to').length).toBe(1);
+  });
+
+  it('offset works well', () => {
+    const wrapper = mount(
+      <SteppedLineTo
+        from={from}
+        to={to}
+        key="line-test"
+        orientation="v"
+        offset={{ x: 100, y: 100 }}
+      />,
+    );
+    expect(wrapper).toMatchSnapshot();
+
+    const x0 = wrapper
+      .find('SteppedLine')
+      .at(0)
+      .prop('x0');
+    expect(x0 + 100).toBe(0);
+
+    const y0 = wrapper
+      .find('SteppedLine')
+      .at(0)
+      .prop('y0');
+    expect(y0 + 100).toBe(0);
+  });
+});
--- src/tools/__tests__/Toolbar.test.js
+++ src/tools/__tests__/Toolbar.test.js
@@ -4,10 +4,13 @@ const testDist = process.env.LIB_DIR === 'dist';
 const Toolbar = testDist
   ? require('../../../dist/lib/toolbar').default
   : require('../Toolbar').default;
+import { tools } from '../../options/tools';
 import 'nino-cli/scripts/setup';
 
 describe('Toolbar', () => {
   it('toolbar render correctly', () => {
-    expect(mount(<Toolbar />)).toMatchSnapshot();
+    const wrapper = mount(<Toolbar />);
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('.anticon').length).toBe(tools.length);
   });
 });
