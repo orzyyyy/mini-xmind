ca48af0d2b54cf746cecdd6f090977b12eb7176c
zy410419243 <zy410419243@gmail.com>
2019-02-04 15:16:14 +0800

    fix: throw error correctly by dekko

--- scripts/dekko-dist.js
+++ scripts/dekko-dist.js
@@ -1,10 +1,12 @@
 const $ = require('dekko');
 const chalk = require('chalk');
 
-$('dist/lib')
-  .isDirectory()
-  .hasFile('canvas.js')
-  .hasFile('toolbar.js');
-
-// eslint-disable-next-line
-console.log(chalk.green('✨ `dist` directory is valid.'));
+try {
+  $('dist/lib')
+    .isDirectory()
+    .hasFile('canvas.js')
+    .hasFile('toolbar.js');
+} catch (error) {
+  // eslint-disable-next-line
+  console.log(chalk.red('✨ ' + error));
+}
--- scripts/dekko-lib.js
+++ scripts/dekko-lib.js
@@ -1,14 +1,16 @@
 const $ = require('dekko');
 const chalk = require('chalk');
 
-$('lib')
-  .isDirectory()
-  .hasFile('index.js')
-  .hasDirectory('canvas')
-  .hasDirectory('line')
-  .hasDirectory('options')
-  .hasDirectory('tools')
-  .hasDirectory('utils');
-
-// eslint-disable-next-line
-console.log(chalk.green('✨ `lib` directory is valid.'));
+try {
+  $('lib')
+    .isDirectory()
+    .hasFile('index.js')
+    .hasDirectory('canvas')
+    .hasDirectory('line')
+    .hasDirectory('options')
+    .hasDirectory('tools')
+    .hasDirectory('utils');
+} catch (error) {
+  // eslint-disable-next-line
+  console.log(chalk.red('✨ ' + error));
+}
