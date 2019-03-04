f3ee746c65b5df8b3855fa36e956d2c1ad5aad4e
zy410419243 <zy410419243@gmail.com>
2019-03-04 09:02:11 +0800

    test: update cases for Toolbar

--- src/tools/__tests__/Toolbar.test.tsx
+++ src/tools/__tests__/Toolbar.test.tsx
@@ -18,4 +18,16 @@ describe('Toolbar', () => {
     expect(wrapper).toMatchSnapshot();
     expect(wrapper.find('.anticon').length).toBe(tools.length);
   });
+
+  it('onDragStart', () => {
+    const wrapper: any = mount(<Toolbar />).instance();
+    const onChange = jest.fn();
+    const setData = function() {
+      onChange();
+    };
+    const event = { dataTransfer: { effectAllowed: '', setData } };
+    wrapper.onDragStart(event, '{test:1}');
+    expect(onChange).toBeCalled();
+    expect(event.dataTransfer.effectAllowed).toBe('copy');
+  });
 });
