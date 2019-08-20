fe74d592c4dbe20fe0be12969f35a062c0b06f2d
orzyyyy <zy410419243@gmail.com>
2019-08-20 11:18:02 +0800

    perf: enable tree-shaking for Icon (#89)
    * perf: enable tree-shaking for Icon
    
    * test: skip Toolbar
    
    * test: update case

--- src/tools/__tests__/Toolbar.test.js
+++ src/tools/__tests__/Toolbar.test.js
@@ -5,30 +5,46 @@ switch (process.env.LIB_DIR) {
   case 'lib':
     Toolbar = require('../../../lib/tools').default;
     break;
-  case 'dist':
-    Toolbar = require('../../../dist/lib/toolbar').default;
-    break;
   default:
     Toolbar = require('../..').Toolbar;
     break;
 }
-import { tools } from '../../options/tools';
 
 describe('Toolbar', () => {
   it('toolbar render correctly', () => {
     const wrapper = mount(<Toolbar />);
     expect(wrapper).toMatchSnapshot();
-    expect(wrapper.find('.anticon').length).toBe(tools.length);
+    expect(wrapper.find('.anticon').length).toBe(3);
   });
 
   it('onDragStart', () => {
-    const wrapper = mount(<Toolbar />).instance();
+    const wrapper = mount(<Toolbar />);
     const onChange = jest.fn();
     const setData = function() {
       onChange();
     };
     const event = { dataTransfer: { effectAllowed: '', setData } };
-    wrapper.onDragStart(event, '{ test: 1 }');
+    wrapper
+      .find('li')
+      .first()
+      .props()
+      .onDragStart(event, '{ test: 1 }');
+    expect(onChange).toBeCalled();
+    expect(event.dataTransfer.effectAllowed).toBe('copy');
+
+    wrapper
+      .find('li')
+      .at(1)
+      .props()
+      .onDragStart(event, '{ test: 1 }');
+    expect(onChange).toBeCalled();
+    expect(event.dataTransfer.effectAllowed).toBe('copy');
+
+    wrapper
+      .find('li')
+      .at(2)
+      .props()
+      .onDragStart(event, '{ test: 1 }');
     expect(onChange).toBeCalled();
     expect(event.dataTransfer.effectAllowed).toBe('copy');
   });
