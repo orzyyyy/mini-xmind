dea5018d90839b8fca28c075cb96748c649ea27a
只捱宅 <zy410419243@gmail.com>
2019-09-10 20:44:29 +0800

    test: replace js to tsx (#105)

--- src/line/__tests__/Line.test.tsx
+++ src/line/__tests__/Line.test.tsx
@@ -2,23 +2,9 @@ import React from 'react';
 import { mount } from 'enzyme';
 import Line from '../Line';
 
-const from = {
-  width: 0,
-  height: 0,
-  left: 0,
-  top: 0,
-};
-
-const to = {
-  width: 0,
-  height: 0,
-  left: 100,
-  top: 0,
-};
-
 describe('Line', () => {
   it('Line renders correctly', () => {
-    const wrapper = mount(<Line />);
+    const wrapper = mount(<Line x0={0} x1={0} y0={0} y1={0} />);
     expect(wrapper).toMatchSnapshot();
   });
 });
