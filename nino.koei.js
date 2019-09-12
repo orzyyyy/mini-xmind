04e272ad80354e2d5ef90bf6f05ba5026086b39c
orzyyyy <zy410419243@gmail.com>
2019-09-12 08:56:24 +0800

    feat: build for dist

--- nino.koei.js
+++ nino.koei.js
@@ -5,24 +5,18 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
   mode: 'production',
   entry: {
-    'lib/canvas': path.join(cwd + '/src/canvas/core'),
-    'lib/toolbar': path.join(cwd + '/src/tools/Toolbar'),
     ninoninoni: path.join(cwd, '/src/go'),
   },
   output: {
-    path: path.join(cwd + '/dist'),
+    path: path.join(cwd + '/site'),
     filename: '[name].js',
     libraryTarget: 'umd',
   },
   plugins: [
     new CopyWebpackPlugin([
       {
-        from: path.join(cwd, '.circleci/config.yml'),
-        to: path.join(cwd, 'dist/.circleci/config.yml'),
-      },
-      {
         from: path.join(cwd, 'src/favicon.ico'),
-        to: path.join(cwd, 'dist/favicon.ico'),
+        to: path.join(cwd, 'site/favicon.ico'),
       },
     ]),
   ],
