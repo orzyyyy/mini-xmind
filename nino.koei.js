478149a9c5806ca178211ca975e9cf079d5b856d
orzyyyy <zy410419243@gmail.com>
2020-08-26 10:13:34 +0800

    fix: update config for copy-webpack-plugin@6.x

--- nino.koei.js
+++ nino.koei.js
@@ -1,27 +1,29 @@
-const path = require('path');
+const path = require("path");
 const cwd = process.cwd();
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  mode: 'production',
+  mode: "production",
   entry: {
-    ninoninoni: path.join(cwd, '/src/go'),
+    ninoninoni: path.join(cwd, "/src/go"),
   },
   output: {
-    path: path.join(cwd + '/site'),
-    filename: '[name].js',
-    libraryTarget: 'umd',
+    path: path.join(cwd + "/site"),
+    filename: "[name].js",
+    libraryTarget: "umd",
   },
   plugins: [
-    new CopyWebpackPlugin([
-      {
-        from: path.join(cwd, 'src/favicon.ico'),
-        to: path.join(cwd, 'site/favicon.ico'),
-      },
-    ]),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.join(cwd, "src/favicon.ico"),
+          to: path.join(cwd, "site/favicon.ico"),
+        },
+      ],
+    }),
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: "./src/index.html",
       hash: true,
       minify: {
         minifyJS: true,
