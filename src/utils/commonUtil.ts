04eb46703649622330c99ac45b245d5d37df9f35
orzyyyy <zy410419243@gmail.com>
2021-09-11 13:20:08 +0800

    chore: bump typescript to 4.4.0

--- src/utils/commonUtil.ts
+++ src/utils/commonUtil.ts
@@ -1,12 +1,12 @@
-import { LineItem } from '../tools/LineGroup';
+import { LineItem } from "../tools/LineGroup";
 
 export function noop() {}
 
-export const convertDomRect2Object = (item: DOMRect | ClientRect): LineItem => {
+export const convertDomRect2Object = (item: DOMRect): LineItem => {
   let x;
   let y;
-  x = 'x' in item ? item.x : item.left;
-  y = 'y' in item ? item.y : item.top;
+  x = "x" in item ? item.x : item.left;
+  y = "y" in item ? item.y : item.top;
   return {
     top: item.top,
     right: item.right,
